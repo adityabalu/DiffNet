fd6c61fab9abefcdd4c1a736ef1016209db57dd0
Biswajit <biswajitkhara@gmail.com>
2022-03-13 17:01:46 -0500

    Made the Stokes-LDC standalone along with experimental changes to its parameters and optimization. Seems to work, but need more experiments.

--- examples/stokes/single_instance/e2_stokes_ldc_resmin.py
+++ examples/stokes/single_instance/e2_stokes_ldc_resmin.py
@@ -24,7 +24,7 @@ seed_everything(42)
 import DiffNet
 from DiffNet.DiffNetFEM import DiffNet2DFEM
 from torch.utils import data
-from e1_stokes_base_resmin import Stokes2D
+# from e1_stokes_base_resmin import Stokes2D
 
 class Stokes_LDC_Dataset(data.Dataset):
     'PyTorch dataset for Stokes_MMS_Dataset'
@@ -69,10 +69,26 @@ class Stokes_LDC_Dataset(data.Dataset):
         forcing = np.ones_like(self.x)*(1/self.Re)
         return torch.FloatTensor(inputs), torch.FloatTensor(forcing).unsqueeze(0)
 
-class Stokes_LDC(Stokes2D):
+class Stokes_LDC(DiffNet2DFEM):
     """docstring for Stokes_LDC"""
     def __init__(self, network, dataset, **kwargs):
-        super(Stokes_LDC, self).__init__(network, dataset, **kwargs)
+        super(Stokes_LDC, self).__init__(network[0], dataset, **kwargs)
+
+        self.net_u = network[0]
+        self.net_v = network[1]
+        self.net_p = network[2]
+
+        self.Re = self.dataset.Re
+        self.pspg_param = self.h**2 * self.Re / 12.
+
+        ue, ve, pe = self.exact_solution(self.dataset.x, self.dataset.y)
+        self.u_exact = torch.FloatTensor(ue)
+        self.v_exact = torch.FloatTensor(ve)
+        self.p_exact = torch.FloatTensor(pe)
+
+        fx_gp, fy_gp = self.forcing(self.xgp, self.ygp)
+        self.fx_gp = torch.FloatTensor(fx_gp)
+        self.fy_gp = torch.FloatTensor(fy_gp)
 
         u_bc = np.zeros_like(self.dataset.x); u_bc[-1,:] = 1. - 4. * (self.dataset.x[-1,:]-0.5)**2
         v_bc = np.zeros_like(self.dataset.x)
@@ -82,44 +98,193 @@ class Stokes_LDC(Stokes2D):
         self.v_bc = torch.FloatTensor(v_bc)
         self.p_bc = torch.FloatTensor(p_bc)
 
+        numerical = np.loadtxt('stokes-ldc-numerical-results/midline_cuts_Re1_128x128.txt', delimiter=",", skiprows=1)
+        self.midline_X = numerical[:,0]
+        self.midline_Y = numerical[:,0]
+        self.midline_U = numerical[:,1]
+        self.midline_V = numerical[:,2]
+
+    def exact_solution(self, x, y):
+        print("exact_solution -- LDC class called")
+        u_exact = np.zeros_like(x)
+        v_exact = np.zeros_like(x)
+        p_exact = np.zeros_like(x)
+        return u_exact, v_exact, p_exact
+
+    def forcing(self, x, y):
+        print("forcing -- LDC class called")
+        fx = np.zeros_like(x)
+        fy = np.zeros_like(x)
+        return fx, fy
+
+    def Q1_vector_assembly(self, Aglobal, Aloc_all):
+        Aglobal[:,0, 0:-1, 0:-1] += Aloc_all[:,0, :, :]
+        Aglobal[:,0, 0:-1, 1:  ] += Aloc_all[:,1, :, :]
+        Aglobal[:,0, 1:  , 0:-1] += Aloc_all[:,2, :, :]
+        Aglobal[:,0, 1:  , 1:  ] += Aloc_all[:,3, :, :]
+        return Aglobal
+
+    def calc_residuals(self, pred, inputs_tensor, forcing_tensor):
+        N_values = self.Nvalues.type_as(pred[0])
+        dN_x_values = self.dN_x_values.type_as(pred[0])
+        dN_y_values = self.dN_y_values.type_as(pred[0])
+        gpw = self.gpw.type_as(pred[0])
+
+        fx_gp = self.fx_gp.type_as(pred[0])
+        fy_gp = self.fy_gp.type_as(pred[0])
+
+        u_bc = self.u_bc.unsqueeze(0).unsqueeze(0).type_as(pred[0])
+        v_bc = self.v_bc.unsqueeze(0).unsqueeze(0).type_as(pred[0])
+        p_bc = self.p_bc.unsqueeze(0).unsqueeze(0).type_as(pred[0])
+
+
+        f = forcing_tensor # renaming variable
+
+        u = pred[0] #[:,0:1,:,:]
+        v = pred[1] #[:,1:2,:,:]
+        p = pred[2] #[:,2:3,:,:]
+
+        # extract diffusivity and boundary conditions here
+        x = inputs_tensor[:,0:1,:,:]
+        y = inputs_tensor[:,1:2,:,:]
+        bc1 = inputs_tensor[:,2:3,:,:]
+        bc2 = inputs_tensor[:,3:4,:,:]
+        bc3 = inputs_tensor[:,4:5,:,:]
+
+        # DERIVE NECESSARY VALUES
+        trnsfrm_jac = (0.5*self.h)**2
+        JxW = (gpw*trnsfrm_jac).unsqueeze(-1).unsqueeze(-1).unsqueeze(0)
+
+        # apply boundary conditions
+        u = torch.where(bc1>=0.5, u_bc, u)
+        v = torch.where(bc2>=0.5, v_bc, v)
+        p = torch.where(bc3>=0.5, p_bc, p)
+        
+        u_gp = self.gauss_pt_evaluation(u)
+        v_gp = self.gauss_pt_evaluation(v)
+        p_gp = self.gauss_pt_evaluation(p)
+        p_x_gp = self.gauss_pt_evaluation_der_x(p)
+        p_y_gp = self.gauss_pt_evaluation_der_y(p)
+        u_x_gp = self.gauss_pt_evaluation_der_x(u)
+        u_y_gp = self.gauss_pt_evaluation_der_y(u)
+        v_x_gp = self.gauss_pt_evaluation_der_x(v)
+        v_y_gp = self.gauss_pt_evaluation_der_y(v)
+
+        # CALCULATION STARTS
+        # lhs
+        W_U1x = N_values*u_x_gp*JxW
+        W_U2y = N_values*v_y_gp*JxW
+        Wx_U1x = dN_x_values*u_x_gp*JxW
+        Wy_U1y = dN_y_values*u_y_gp*JxW
+        Wx_U2x = dN_x_values*v_x_gp*JxW
+        Wy_U2y = dN_y_values*v_y_gp*JxW
+        Wx_P = dN_x_values*p_gp*JxW
+        Wy_P = dN_y_values*p_gp*JxW
+        Wx_Px = dN_x_values*p_x_gp*JxW
+        Wy_Py = dN_y_values*p_y_gp*JxW
+        # rhs
+        W_F1 = N_values*fx_gp*JxW
+        W_F2 = N_values*fy_gp*JxW
+
+        # integrated values on lhs & rhs
+        temp1 = self.dataset.Re*(Wx_U1x+Wy_U1y) - Wx_P - W_F1
+        temp2 = self.dataset.Re*(Wx_U2x+Wy_U2y) - Wy_P - W_F2
+        temp3 = W_U1x+W_U2y + self.pspg_param*(Wx_Px+Wy_Py)
+
+        # unassembled residual
+        R_split_1 = torch.sum(temp1, 2) # sum across all GP
+        R_split_2 = torch.sum(temp2, 2) # sum across all GP
+        R_split_3 = torch.sum(temp3, 2) # sum across all GP
+
+        # assembly
+        R1 = torch.zeros_like(u); R1 = self.Q1_vector_assembly(R1, R_split_1)
+        R2 = torch.zeros_like(u); R2 = self.Q1_vector_assembly(R2, R_split_2)
+        R3 = torch.zeros_like(u); R3 = self.Q1_vector_assembly(R3, R_split_3)
+
+        # add boundary conditions to R <---- this step is very important
+        R1 = torch.where(bc1>=0.5, u_bc, R1)
+        R2 = torch.where(bc2>=0.5, v_bc, R2)
+        R3 = torch.where(bc3>=0.5, p_bc, R3)
+
+        return R1, R2, R3
+
     def loss(self, pred, inputs_tensor, forcing_tensor):
         R1, R2, R3 = self.calc_residuals(pred, inputs_tensor, forcing_tensor)
         # loss = torch.norm(R1, 'fro') + torch.norm(R2, 'fro') + torch.norm(R3, 'fro')
         return torch.norm(R1, 'fro'), torch.norm(R2, 'fro'), torch.norm(R3, 'fro')
 
+    def forward(self, batch):
+        inputs_tensor, forcing_tensor = batch
+        return self.net_u[0], self.net_v[0], self.net_p[0], inputs_tensor, forcing_tensor
+
     def training_step(self, batch, batch_idx, optimizer_idx):
-        u, inputs_tensor, forcing_tensor = self.forward(batch)
-        loss_vals = self.loss(u, inputs_tensor, forcing_tensor)
-        # self.log('PDE_loss', sum(loss_vals).item())
+        u, v, p, inputs_tensor, forcing_tensor = self.forward(batch)
+        loss_vals = self.loss((u, v, p), inputs_tensor, forcing_tensor)
         self.log('loss_u', loss_vals[0].item())
         self.log('loss_v', loss_vals[1].item())
         self.log('loss_p', loss_vals[2].item())
-        # return loss_vals[optimizer_idx]
         return {"loss": loss_vals[optimizer_idx]}
 
     def training_step_end(self, training_step_outputs):
         loss = training_step_outputs["loss"]
-        # unorm = training_step_outputs["unorm"]
-        # self.log('loss', loss.item())
-        # self.log('unorm', unorm.item())
         return training_step_outputs
 
     def configure_optimizers(self):
-        # print("self.network[0].parameters() = ", self.network[0].parameters())
         lr = self.learning_rate
         # opts = [torch.optim.LBFGS(self.network, lr=1.0, max_iter=5)]
-        opts = [torch.optim.Adam(self.network, lr=lr), torch.optim.Adam(self.network, lr=lr), torch.optim.Adam(self.network, lr=lr)]
-        # opts = [torch.optim.Adam(self.network, lr=lr), torch.optim.LBFGS(self.network, lr=1.0, max_iter=5)]
+        opts = [torch.optim.Adam(self.net_u, lr=lr), torch.optim.Adam(self.net_v, lr=lr), torch.optim.Adam(self.net_p, lr=lr)]
         return opts, []
+
+    def on_epoch_end(self):
+        # self.network.eval()
+        self.net_u.eval()
+        self.net_v.eval()
+        self.net_p.eval()
+        inputs, forcing = self.dataset[0]
+        u, v, p, u_x, v_y = self.do_query(inputs, forcing)
+
+        u = u.squeeze().detach().cpu()
+        v = v.squeeze().detach().cpu()
+        p = p.squeeze().detach().cpu()
+        u_x = u_x.squeeze().detach().cpu()
+        v_y = v_y.squeeze().detach().cpu()
+
+        self.plot_contours(u, v, p, u_x, v_y)
+
+    def do_query(self, inputs, forcing):
+        u, v, p, inputs_tensor, forcing_tensor = self.forward((inputs.unsqueeze(0).type_as(next(self.net_u.parameters())), forcing.unsqueeze(0).type_as(next(self.net_u.parameters()))))
+
+        f = forcing_tensor # renaming variable
+
+        # extract diffusivity and boundary conditions here
+        x = inputs_tensor[:,0:1,:,:]
+        y = inputs_tensor[:,1:2,:,:]
+        bc1 = inputs_tensor[:,2:3,:,:]
+        bc2 = inputs_tensor[:,3:4,:,:]
+        bc3 = inputs_tensor[:,4:5,:,:]
+
+        # apply boundary conditions
+        u_bc = self.u_bc.unsqueeze(0).unsqueeze(0).type_as(u)
+        v_bc = self.v_bc.unsqueeze(0).unsqueeze(0).type_as(u)
+        p_bc = self.p_bc.unsqueeze(0).unsqueeze(0).type_as(u)
+
+        u = torch.where(bc1>=0.5, u_bc, u)
+        v = torch.where(bc2>=0.5, v_bc, v)
+        p = torch.where(bc3>=0.5, p_bc, p)
+
+        u_x = self.gauss_pt_evaluation_der_x(u)[:,0,:,:]
+        v_y = self.gauss_pt_evaluation_der_y(v)[:,0,:,:]
+
+        return u, v, p, u_x, v_y
 
     def plot_contours(self, u, v, p, u_x, v_y):
         fig, axs = plt.subplots(3, 3, figsize=(4*3,2.4*3),
                             subplot_kw={'aspect': 'auto'}, squeeze=True)
 
-        for ax_row in axs:
-            for ax in ax_row:
-                ax.set_xticks([])
-                ax.set_yticks([]) 
+        for i in range(axs.shape[0]-1):
+            for j in range(axs.shape[1]):
+                axs[i,j].set_xticks([])
+                axs[i,j].set_yticks([])
         
         div = u_x + v_y
 
@@ -132,69 +297,49 @@ class Stokes_LDC(Stokes2D):
         x = np.linspace(0, 1, u.shape[0])
         y = np.linspace(0, 1, u.shape[1])
 
-        # im = axs[1,0].imshow(u-self.u_exact.numpy(),cmap='jet', origin='lower')
-        # fig.colorbar(im, ax=axs[1,0])
-        # im = axs[1,1].imshow(v-self.v_exact.numpy(),cmap='jet',origin='lower')
-        # fig.colorbar(im, ax=axs[1,1])  
-        # im = axs[1,2].imshow(p-self.p_exact.numpy(),cmap='jet',origin='lower')
-        # fig.colorbar(im, ax=axs[1,2])
-
         im3 = axs[1,0].imshow(div,cmap='jet',origin='lower')
         fig.colorbar(im3, ax=axs[1,0])  
         im4 = axs[1,1].imshow((u**2 + v**2)**0.5,cmap='jet',origin='lower')
         fig.colorbar(im4, ax=axs[1,1])
         xx , yy = np.meshgrid(x, y)
-        im5 = axs[1,2].streamplot(xx, yy, u, v, color='k', cmap='jet')        
-
-        baseline_cut = np.array([
-                [0.0032066932270914394, -0.0007171314741036827],
-                [-0.08300988047808777, 0.15482071713147427],
-                [-0.12839219123505985, 0.2657370517928288],
-                [-0.15676031872509966, 0.3358565737051793],
-                [-0.18517529880478095, 0.4149003984063745],
-                [-0.20523043824701204, 0.501593625498008],
-                [-0.20285211155378485, 0.5819123505976096],
-                [-0.15568717131474114, 0.664780876494024],
-                [-0.06084860557768934, 0.7336254980079682],
-                [0.08302342629482051, 0.7960956175298806],
-                [0.2402690836653384, 0.8445418326693228],
-                [0.40455490039840614, 0.8853386454183267],
-                [0.5471942629482069, 0.9159362549800798],
-                [0.670280478087649, 0.9376095617529882],
-                [0.7653800796812744, 0.9567330677290837],
-                [0.8395198406374498, 0.9682071713147412],
-                [0.9961859760956173, 0.9937051792828686]
-            ])
+        im5 = axs[1,2].streamplot(xx, yy, u, v, color='k', cmap='jet')
 
         mid_idx = int(self.domain_size/2)
         im = axs[2,0].plot(self.dataset.y[:,mid_idx], u[:,mid_idx],label='DiffNet')
-        im = axs[2,0].plot(baseline_cut[:,1], baseline_cut[:,0],label='Ghia')
+        im = axs[2,0].plot(self.midline_Y,self.midline_U,label='Numerical')
         axs[2,0].legend()
-        # im = axs[1,1].imshow(v-self.v_exact.numpy(),cmap='jet',origin='lower')
-        # fig.colorbar(im, ax=axs[1,1])          
+        im = axs[2,1].plot(self.dataset.x[mid_idx,:], v[mid_idx,:],label='DiffNet')
+        im = axs[2,1].plot(self.midline_X,self.midline_V,label='Numerical')
+        axs[2,1].legend()
 
         plt.savefig(os.path.join(self.logger[0].log_dir, 'contour_' + str(self.current_epoch) + '.png'))
         self.logger[0].experiment.add_figure('Contour Plots', fig, self.current_epoch)
         plt.close('all')
 
 def main():
-    domain_size = 64
+    domain_size = 32
+    Re = 1.
     dir_string = "stokes_ldc"
-    max_epochs = 1000
+    max_epochs = 150
+    LR = 1e-2
 
     x = np.linspace(0, 1, domain_size)
     y = np.linspace(0, 1, domain_size)
     xx , yy = np.meshgrid(x, y)
 
-    dataset = Stokes_LDC_Dataset(domain_size=domain_size)
-    v1 = np.zeros_like(dataset.x) # np.sin(math.pi*xx)*np.cos(math.pi*yy)
-    v2 = np.zeros_like(dataset.x) # -np.cos(math.pi*xx)*np.sin(math.pi*yy)
-    p  = np.zeros_like(dataset.x) # np.sin(math.pi*xx)*np.sin(math.pi*yy)
+    dataset = Stokes_LDC_Dataset(domain_size=domain_size, Re=Re)
+    v1 = np.zeros_like(dataset.x)
+    v2 = np.zeros_like(dataset.x)
+    p  = np.zeros_like(dataset.x)
     u_tensor = np.expand_dims(np.array([v1,v2,p]),0)
-    print(u_tensor.shape)
-    network = torch.nn.ParameterList([torch.nn.Parameter(torch.FloatTensor(u_tensor), requires_grad=True)])
-    print("network = ", network)
-    basecase = Stokes_LDC(network, dataset, domain_size=domain_size, batch_size=1, fem_basis_deg=1, learning_rate=0.001)
+    
+    # network = torch.nn.ParameterList([torch.nn.Parameter(torch.FloatTensor(u_tensor), requires_grad=True)])
+    net_u = torch.nn.ParameterList([torch.nn.Parameter(torch.FloatTensor(u_tensor[:,0:1,:,:]), requires_grad=True)])
+    net_v = torch.nn.ParameterList([torch.nn.Parameter(torch.FloatTensor(u_tensor[:,1:2,:,:]), requires_grad=True)])
+    net_p = torch.nn.ParameterList([torch.nn.Parameter(torch.FloatTensor(u_tensor[:,2:3,:,:]), requires_grad=True)])
+    network = (net_u, net_v, net_p)
+
+    basecase = Stokes_LDC((net_u, net_v, net_p), dataset, domain_size=domain_size, batch_size=1, fem_basis_deg=1, learning_rate=LR)
 
     # Initialize trainer
     logger = pl.loggers.TensorBoardLogger('.', name=dir_string)
@@ -213,7 +358,9 @@ def main():
     # Training
     trainer.fit(basecase)
     # Save network
-    torch.save(basecase.network, os.path.join(logger.log_dir, 'network.pt'))
+    torch.save(basecase.net_u, os.path.join(logger.log_dir, 'net_u.pt'))
+    torch.save(basecase.net_v, os.path.join(logger.log_dir, 'net_v.pt'))
+    torch.save(basecase.net_p, os.path.join(logger.log_dir, 'net_p.pt'))
 
 
 if __name__ == '__main__':
